5609021216f3adc777ed4642e2703fea52d5f613
parth448812 <parthkpatel04@gmail.com>
2024-01-30 21:36:58 -0500

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,29 +1,22 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+const budget = require('./budget-data.json');
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
+
+// app.get('/budget', (req, res) => {
+//     fs.readFile('budget-data.json', 'utf8', (data) => {
+//         const budget = JSON.parse(data);
+//             res.json(budget);
+//     });
+// });
 
 app.get('/budget', (req, res) => {
     res.json(budget);
