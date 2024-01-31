cae1e5932399019da7dffbbbc72fd1ce3297be1e
parth448812 <parthkpatel04@gmail.com>
2024-01-30 23:03:58 -0500

    Added D3JS chart

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
 const port = 3000;
 
@@ -11,12 +10,7 @@ app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
-// app.get('/budget', (req, res) => {
-//     fs.readFile('budget-data.json', 'utf8', (data) => {
-//         const budget = JSON.parse(data);
-//             res.json(budget);
-//     });
-// });
+
 
 app.get('/budget', (req, res) => {
     res.json(budget);
